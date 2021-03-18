60a8a690ab05a50dc769ea10e25cae9f30762953
youjeong-lee <dyeudsla@gmail.com>
2021-03-18 21:06:12 +0900

    complete build weather app with expo

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Alert } from "react-native";
+import * as Location from "expo-location";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Loading from "./loading";
+import Weather from "./Weather";
+
+const API_KEY = "APP_KEY";
+
+export default class App extends React.Component {
+	// state [START]
+	state = {
+		isLoading: true,
+		temp: 0,
+		condition: "",
+	};
+	// state [END]
+	// getWeather[START]
+	getWeather = async (latitude, longitude) => {
+		// Send to API and get weather!
+		const {
+			data: { main, weather },
+		} = await axios(
+			`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+		);
+		this.setState({ isLoading: false, temp: main.temp, condition: weather[0].main });
+	};
+	// getWeather[END]
+	// getLocation [START]
+	getLocation = async () => {
+		try {
+			await Location.requestPermissionsAsync();
+			const {
+				coords: { latitude, longitude },
+			} = await Location.getCurrentPositionAsync();
+
+			// getWeather
+			this.getWeather(latitude, longitude);
+		} catch (error) {
+			Alert.alert("Can't find you.", "So sad...!");
+		}
+	};
+	// getLocation [END]
+
+	// componentDidMount [START]
+	componentDidMount() {
+		this.getLocation();
+	}
+	// componentDidMount [END]
+
+	// render[START]
+	render() {
+		const { isLoading, temp, condition } = this.state;
+		return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
+	}
+	// render[END]
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
